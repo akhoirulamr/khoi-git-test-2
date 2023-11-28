81e91da182221fdbf572d5eb0dde0a73523c2e91
Afif Khoirul Amri <khoirulamr9@gmail.com>
2023-11-28 18:53:57 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h3>hellow worls</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus veritatis praesentium iste repellat accusantium eveniet deleniti eos reprehenderit saepe perspiciatis labore earum, fugiat magni asperiores quis neque delectus iure vero.</p>
 </body>
 </html>
