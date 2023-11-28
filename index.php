33ede8a363cff2477a88ca18426b3b55341896d4
Afif Khoirul Amri <khoirulamr9@gmail.com>
2023-11-28 18:58:22 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h3>hellow worls</h3>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus veritatis praesentium iste repellat accusantium eveniet deleniti eos reprehenderit saepe perspiciatis labore earum, fugiat magni asperiores quis neque delectus iure vero.</p>
+    <ul>list</ul>
 </body>
 </html>
